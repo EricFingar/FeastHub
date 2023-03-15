cd9e889b3fee4cf7a6fc1aec7b1fb0da39dc1402
Alek Ahrens <scaaja@gmail.com>
2023-03-14 21:13:54 -0400

    making the add recipe page allow you to have multiple lines that the user can add

--- app/src/main/java/com/example/feasthub/AddReceiptsFragment.java
+++ app/src/main/java/com/example/feasthub/AddReceiptsFragment.java
@@ -1,35 +1,42 @@
 package com.example.feasthub;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.media.Image;
 import android.os.Bundle;
 
-import androidx.core.content.ContextCompat;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddReceiptsFragment extends Fragment {
     private View view;
     private ImageView image;
+    private ListView ingList;
+    private List<String> list;
+    private ArrayAdapter<String> arrayAdapter;
+
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_add_receipts, container, false);
+
         submitButton();
         return view;
     }
+
 
     private void submitButton(){
         Button submitButton = (Button) view.findViewById(R.id.addRecipeSubmitButton);
@@ -41,9 +48,6 @@ public class AddReceiptsFragment extends Fragment {
             }
         });
     }
-
-
-
 
 
 }
